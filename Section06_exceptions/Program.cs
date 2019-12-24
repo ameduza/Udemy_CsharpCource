5d0724197fe5eaa2154e9d8fb5e4890b80913322
alexander.maximov <alexander.maximov@arcadia.spb.ru>
2019-12-24 23:02:05 +0300

    Add dummy project for exceptions

---
+++ Section06_exceptions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Section06_exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
