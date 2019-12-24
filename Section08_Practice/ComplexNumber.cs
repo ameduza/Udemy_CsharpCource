a45579fb23ab59b783da7b1435dd47a15f278c87
alexander.maximov <alexander.maximov@arcadia.spb.ru>
2019-12-25 00:07:39 +0300

    ComplexNumbers homework

---
+++ Section08_Practice/ComplexNumber.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Section08_Practice
+{
+    public class ComplexNumber
+    {
+        public ComplexNumber(double realPart, double imaginaryPart)
+        {
+            RealPart = realPart;
+            ImaginaryPart = imaginaryPart;
+        }
+        private ComplexNumber()
+        {
+        }
+        public double RealPart { get; private set; }
+        public double ImaginaryPart { get; private set; }
+
+        internal ComplexNumber Plus(ComplexNumber cn2)
+        { // My implementation
+            double realPart = RealPart + cn2.RealPart;
+            double imaginaryPart = ImaginaryPart + cn2.ImaginaryPart;
+            
+            return new ComplexNumber(realPart, imaginaryPart);
+        }
+
+        internal ComplexNumber Minus(ComplexNumber other)
+        { // Fofanov implementation
+            ComplexNumber result = new ComplexNumber();
+            result.RealPart = RealPart - other.RealPart;
+            result.ImaginaryPart = ImaginaryPart - other.ImaginaryPart;
+
+            return result;
+        }
+    }
+}
+/*  ДЗ "Комплексные числа"
+Разработать класс представляющий комплексное число. Класс должен содержать два свойства для представления вещестенной (double) и мнимой части (double). 
+Сделать так, чтобы создать экземпляр класса без передачи соответствующих аргументов было невозможно.
+
+Создать два метода, реализующих сложение и вычитание двух комплексных чисел. Чтобы сложить два комплексных числа необходимо 
+по отдельности сложить их вещественные и мнимые части.
+То есть, предположим, что мы имеет два комплексных числа. У первого вещественная часть равна 1, мнимая 2. У второго вещественная часть равна 3, мнимая 1. 
+Результатам будет комплексное число, где вещественная часть равна 1+3=4, а мнимая равна 2 + 1 = 3.
+
+Операция вычитания работает по тому же принципу, что и сложение (ну, только вычитание).
+
+API спроектировать таким образом, чтобы клиентский код мог написать следующий код:
+
+Complex c1 = new Complex(1, 1);
+Complex c2 = new Complex(1, 2);
+ 
+Complex result = c1.Plus(c2);
+*/
---
+++ Section08_Practice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Section08_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ComplexNumber c1 = new ComplexNumber(5, 3);
+            ComplexNumber c2 = new ComplexNumber(1, 2);
+
+
+            ComplexNumber sumResult = c1.Plus(c2);
+            ComplexNumber substResult = c1.Minus(c2);
+
+            Console.WriteLine($"SumResult = {sumResult.RealPart} + {sumResult.ImaginaryPart}i");
+            Console.WriteLine($"SubstResult = {substResult.RealPart} + {substResult.ImaginaryPart}i");
+
+        }
+    }
+}
+
+
