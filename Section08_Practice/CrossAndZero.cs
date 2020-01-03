f46446c48203ce1dfdb090677985496eeee3ca39
ameduza <ameduza@gmail.com>
2020-01-03 19:06:14 +0300

    Implement Move method. Fix WinnerCalc logic.

--- Section08_Practice/CrossAndZero.cs
+++ Section08_Practice/CrossAndZero.cs
@@ -18,10 +18,10 @@ namespace Section08_Practice
         public CrossAndZero()
         {
             _player = Player.Cross;
-            //_field = new byte?[3, 3] { { null, null, null }, { null, null, null }, { null, null, null } };
+            _field = new byte?[3, 3] { { null, null, null }, { null, null, null }, { null, null, null } };
             //_field = new byte?[3, 3] { { 0, null, null }, { 0, 1, 1 }, { 0, null, null } };
             //_field = new byte?[3, 3] { { 0, null, null }, { 1, 1, 1 }, { 0, null, null } };
-            _field = new byte?[3, 3] { { 0, 1, null }, { 1, 0, 1 }, { 0, null, 0 } };
+            //_field = new byte?[3, 3] { { 1, 0, 1 }, { 1, 0, 0 }, { 0, null, 1 } };
 
             _fieldRowLength = _field.GetLength(0);
             _fieldColumnLength = _field.GetLength(1);
@@ -32,7 +32,7 @@ namespace Section08_Practice
             DrawCurrentState();
             while (_isGameOver == false)
             {
-                // request move
+                Move();
                 DrawCurrentState();
 
                 WinnerCalc();
@@ -43,14 +43,95 @@ namespace Section08_Practice
             Console.WriteLine($"Game is over! Winner is: {winnerNameString}");
         }
 
-        private void DrawCurrentState()
+        private void Move()
         {
             Player playerName = (Player)_player;
             string playerNameString = playerName.ToString();
-
-            Console.Clear();
             Console.WriteLine($"Current move is: {playerNameString}");
             Console.WriteLine();
+
+            byte input = 255;
+            bool isParsed = false, isCellBusy = true;
+            byte r = 10, c = 10;
+            while (!isParsed || input > 8 || isCellBusy)
+            {
+                isParsed = byte.TryParse(Console.ReadLine(), out input);
+                if (!isParsed)
+                    Console.WriteLine("Sorry, can't recognize your input. Please try again with a number between 0 and 8...");
+
+
+                switch (input)
+                {
+                    case 0:
+                        r = 0;
+                        c = 0;
+                        break;
+                    case 1:
+                        r = 0;
+                        c = 1;
+                        break;
+                    case 2:
+                        r = 0;
+                        c = 2;
+                        break;
+                    case 3:
+                        r = 1;
+                        c = 0;
+                        break;
+                    case 4:
+                        r = 1;
+                        c = 1;
+                        break;
+                    case 5:
+                        r = 1;
+                        c = 2;
+                        break;
+                    case 6:
+                        r = 2;
+                        c = 0;
+                        break;
+                    case 7:
+                        r = 2;
+                        c = 1;
+                        break;
+                    case 8:
+                        r = 2;
+                        c = 2;
+                        break;
+
+                    default:
+                        break;
+                }
+
+                if (input > 8)
+                {
+                    Console.WriteLine("The cell number can't be more than 8, please try another value");
+                }
+
+                if (input < 9 &&_field[r, c].HasValue)
+                {
+                    Console.WriteLine($"Cell already has a value. Try another one...");
+                    isCellBusy = true;
+                }
+                else
+                    isCellBusy = false;
+            }
+
+            if (_player == Player.Cross)
+            {
+                _field[r, c] = 1;
+                _player = Player.Zero;
+            }
+            else
+            {
+                _field[r, c] = 0;
+                _player = Player.Cross;
+            }
+        }
+
+        private void DrawCurrentState()
+        {
+            Console.Clear();
 
             string cell;
 
@@ -91,14 +172,14 @@ namespace Section08_Practice
                 }
 
                 byte? rowSum = row[0];
-                for (int i = 0; i < row.Length - 1; i++)
+                for (int i = 1; i < row.Length; i++)
                 {
                     rowSum += row[i];
                 }
 
                 if (rowSum == _fieldRowLength)
                 {
-                    _winner = Player.Cross;
+                     _winner = Player.Cross;
                     _isGameOver = true;
                     return;
                 }
@@ -120,7 +201,7 @@ namespace Section08_Practice
                 }
 
                 byte? columnSum = column[0];
-                for (int i = 0; i < column.Length - 1; i++)
+                for (int i = 1; i < column.Length; i++)
                 {
                     columnSum += column[i];
                 }
@@ -147,7 +228,7 @@ namespace Section08_Practice
 
             if (slashSum == _fieldColumnLength || backSlashSum == _fieldColumnLength)
             {
-                _winner = Player.Cross;
+                 _winner = Player.Cross;
                 _isGameOver = true;
                 return;
             }
@@ -159,6 +240,21 @@ namespace Section08_Practice
             }
 
 
+            //Check if no empty cells
+            byte? fieldCellsSum = 0;
+            for (int row = 0; row < _fieldRowLength; row++)
+            {
+                for (int col = 0; col < _fieldColumnLength; col++)
+                {
+                    fieldCellsSum += _field[row, col];
+                }
+            }
+
+            if (fieldCellsSum != null) 
+            {
+                _isGameOver = true;
+                Console.WriteLine("Game is over! No winner since no empty cells left :(");
+            }
         }
     }
 
