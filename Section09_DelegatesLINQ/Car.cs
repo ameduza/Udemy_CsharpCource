1702eb8bd128c2ee7fecfcde05b27ba2518011b8
ameduza <ameduza@gmail.com>
2020-01-09 19:52:34 +0300

    Move Program  class to separate file

--- Section09_DelegatesLINQ/Car.cs
+++ Section09_DelegatesLINQ/Car.cs
@@ -41,40 +41,4 @@ namespace Section09_DelegatesLINQ
         //}
 
     }
-    class Program
-    {
-        static Car car;
-        static void Main(string[] args)
-        {
-            car = new Car();
-            //car.RegisterOnTooFast(HandleOnTooFast);
-            car.AccelerateEvent += Car_AccelerateEvent;
-            car.TooFastEvent += Car_TooFastEvent;
-            
-
-            car.Start();
-
-            for (int i = 0; i < 10; i++)
-            {
-                car.Accelerate();
-            }
-        }
-
-        private static void Car_AccelerateEvent(int speed)
-        {
-            Console.WriteLine($"Accelerating...Speed = {speed}");
-        }
-
-        private static void Car_TooFastEvent(int speed)
-        {
-            Console.WriteLine($"Your speed is {speed}");
-            car.Stop();
-        }
-
-        //private static void HandleOnTooFast()
-        //{
-        //    Console.WriteLine("Too fast! Slow down!");
-        //    car.Stop();
-        //}
-    }
 }
---
+++ Section09_DelegatesLINQ/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Section09_DelegatesLINQ
+{
+    class Program
+    {
+        static Car car;
+        static void Main(string[] args)
+        {
+
+
+            CarCall();
+
+        }
+
+        private static void CarCall()
+        {
+            car = new Car();
+            //car.RegisterOnTooFast(HandleOnTooFast);
+            car.AccelerateEvent += Car_AccelerateEvent;
+            car.TooFastEvent += Car_TooFastEvent;
+
+
+            car.Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                car.Accelerate();
+            }
+        }
+
+        private static void Car_AccelerateEvent(int speed)
+        {
+            Console.WriteLine($"Accelerating...Speed = {speed}");
+        }
+
+        private static void Car_TooFastEvent(int speed)
+        {
+            Console.WriteLine($"Your speed is {speed}");
+            car.Stop();
+        }
+
+        //private static void HandleOnTooFast()
+        //{
+        //    Console.WriteLine("Too fast! Slow down!");
+        //    car.Stop();
+        //}
+    }
+}
