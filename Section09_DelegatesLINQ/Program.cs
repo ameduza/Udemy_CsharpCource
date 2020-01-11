8cc56120985c47a40dede1df7a6e8037a130f4f8
ameduza <ameduza@gmail.com>
2020-01-11 23:30:54 +0300

    LINQ beginning, Top5Files

--- Section09_DelegatesLINQ/Program.cs
+++ Section09_DelegatesLINQ/Program.cs
@@ -1,16 +1,71 @@
 ﻿using Section09_DelegatesLINQ.SticksGame;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Section09_DelegatesLINQ
 {
+    public static class LinqExt
+    {
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (var file in source)
+            {
+                action(file);
+            }
+        }
+    }
     class Program
     {
         static Car car;
+
         static void Main(string[] args)
         {
-            SticksGameUI.SticksGame();
+            DisplayLargestFilesLinq(@"C:\Distribs");
+            Console.WriteLine("--------------------");
+            DisplayLargestFilesNoLinq(@"C:\Distribs");
+            
+            //SticksGameUI.SticksGame();
 
             //CarCall();
+        }
+
+        private static void DisplayLargestFilesLinq(string pathToDir) 
+        {
+            IEnumerable<FileInfo> topFivefiles = new DirectoryInfo(pathToDir)
+                .GetFiles()
+                .OrderByDescending(x => x.Length)
+                .Take(5);
+
+            foreach (var file in topFivefiles)
+            {
+                Console.WriteLine($"{file.Name} size is {file.Length} bytes");
+            }
+            Console.WriteLine("-------------------- Extention:");
+            
+            topFivefiles
+                .ForEach(file => Console.WriteLine($"{file.Name} size is {file.Length} bytes"));
+        }
+        private static void DisplayLargestFilesNoLinq(string pathToDir)
+        {
+            var dirInfo = new DirectoryInfo(pathToDir);
+            FileInfo[] files = dirInfo.GetFiles();
+
+            Array.Sort(files, FilesSortLengthDesc);
+
+            for (int i = 0; i < 5; i++)
+            {
+                FileInfo file = files[i];
+                Console.WriteLine($"{file.Name} size is {file.Length} bytes");
+            }
+        }
+
+        private static int FilesSortLengthDesc(FileInfo x, FileInfo y)
+        {
+            if (x.Length == y.Length) return 0;
+            if (x.Length < y.Length) return 1;
+            return -1;
         }
 
         private static void CarCall()
